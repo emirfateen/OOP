893bc0f27cd120ac913767bd57506003e6b9e8af
Emir Fateen Haqqi <emir.haqqi@gmail.com>
2023-09-05 19:35:07 +0700

    First Commit

---
+++ testUmur.java
@@ -0,0 +1,13 @@
+public class testUmur {
+  public static void main(String[] args) {
+	String name = "Emir Fateen Haqqi";
+    int umur = 18;
+    System.out.println("Nama: "+ name);
+	System.out.println("Umur: "+ umur);
+    if (umur >= 17) {
+	System.out.println("Sudah punya KTP");
+	} else {
+	System.out.println("Belum punya KTP");
+	}
+  }
+}
---
+++ testoop.java
@@ -0,0 +1,5 @@
+public class testoop{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
